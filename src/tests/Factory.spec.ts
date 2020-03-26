07235b48b3a0c2427236a96690d759d503074819
Agustin <Agusxrun@gmail.com>
2020-03-26 18:16:21 +0100

    Remove WalletFactory helper

--- src/tests/Factory.spec.ts
+++ src/tests/Factory.spec.ts
@@ -3,34 +3,31 @@ import { expect } from './utils';
 ethers.errors.setLogLevel("error")
 
 const Factory = artifacts.require('Factory');
-const WalletFactory = artifacts.require('WalletFactory');
 const ModuleMock = artifacts.require('ModuleMock');
 
 const web3 = (global as any).web3
 
-contract('GoldCardsFactory', (accounts: string[]) => {
+contract('Factory', (accounts: string[]) => {
   let module
   let factory
-  let walletFactory
 
   beforeEach(async () => {
     module        = await ModuleMock.new()
     factory       = await Factory.new()
-    walletFactory = await WalletFactory.new(factory.address, module.address)
   })
 
   describe('Deploy wallets', () => {
     it('Should deploy wallet', async () => {
-      await walletFactory.deploy(accounts[0])
+      await factory.deploy(module.address, accounts[0])
     })
     it('Should predict wallet address', async () => {
-      const predict = await walletFactory.addressOf(accounts[0])
-      await walletFactory.deploy(accounts[0])
+      const predict = await factory.addressOf(module.address, accounts[0])
+      await factory.deploy(module.address, accounts[0])
       expect(await web3.eth.getCode(predict)).to.not.eq("0x")
     })
     it('Should initialize with main module', async () => {
-      await walletFactory.deploy(accounts[0])
-      const wallet = await ModuleMock.at(await walletFactory.addressOf(accounts[0]))
+      await factory.deploy(module.address, accounts[0])
+      const wallet = await ModuleMock.at(await factory.addressOf(module.address, accounts[0]))
       expect((await wallet.ping()).logs[0].event).to.eq("Pong")
     })
   })
