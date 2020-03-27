bac78088fa2dd976ae5767f4f70e75e054046813
Agustin <Agusxrun@gmail.com>
2020-03-27 18:31:04 +0100

    Main module deployer

---
+++ contracts/modules/MainModule.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.5.0;
+
+
+contract MainModule {
+    // keccak256("placeholder-init-code-hash")
+    bytes32 public constant INIT_CODE_HASH = 0xa4e481c95834a9f994a80cd4ecc88bdd3e78ff54100ecf2903aa9ef3eed54a91;
+
+    // keccak256("placeholder-factory")[12:]
+    address public constant FACTORY = address(0x52AA901CAD8AFf3Cf157715c19632F79D9B2d049);
+}
