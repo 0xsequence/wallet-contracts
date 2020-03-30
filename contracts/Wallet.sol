9a00f8eea982328f5733d40db4acb721198e0eee
Agustin <Agusxrun@gmail.com>
2020-03-30 21:05:17 +0200

    Remove hooks from base wallet

--- contracts/Wallet.sol
+++ contracts/Wallet.sol
@@ -2,18 +2,14 @@ pragma solidity ^0.6.4;
 
 
 contract Wallet {
-    address private fallbackModule;
-    mapping(bytes4 => address) private modules;
+    address private implementation;
 
-    constructor(address _fallbackModule) public payable {
-        fallbackModule = _fallbackModule;
+    constructor(address _implementation) public payable {
+        implementation = _implementation;
     }
 
     fallback() external {
-        address target = modules[msg.sig];
-        if (target == address(0)) target = fallbackModule;
-
-        (bool result, bytes memory data) = target.delegatecall(msg.data);
+        (bool result, bytes memory data) = implementation.delegatecall(msg.data);
         require(result, string(data));
     }
 }
