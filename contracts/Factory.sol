c1867b9472d6a73a1844e02c1815a82c9b2ba4a7
Agustin <Agusxrun@gmail.com>
2020-03-31 19:38:03 +0200

    Reimplement Wallet using assembly

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -4,12 +4,12 @@ import "./Wallet.sol";
 
 contract Factory {
   function deploy(address _mainModule, bytes32 _salt) public payable returns (address _contract) {
-    bytes memory code = abi.encodePacked(type(Wallet).creationCode, uint256(_mainModule));
+    bytes memory code = abi.encodePacked(Wallet.creationCode, uint256(_mainModule));
     assembly { _contract := create2(callvalue(), add(code, 32), mload(code), _salt) }
   }
 
   function addressOf(address _mainModule, bytes32 _salt) external view returns (address) {
-    bytes memory code = abi.encodePacked(type(Wallet).creationCode, uint256(_mainModule));
+    bytes memory code = abi.encodePacked(Wallet.creationCode, uint256(_mainModule));
 
     return address(
       uint256(
