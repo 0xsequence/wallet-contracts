3356f282d12082c5313acbd9f8e1b11ed1abbec6
Agustin <Agusxrun@gmail.com>
2020-04-01 18:59:42 +0200

    Remove RegEx from RevertError

--- src/tests/utils/helpers.ts
+++ src/tests/utils/helpers.ts
@@ -18,7 +18,7 @@ export const createTestWallet = (web3: any, addressIndex: number = 0) => {
 // Check if tx was Reverted with specified message
 export function RevertError(errorMessage?: string) {
   let prefix = 'VM Exception while processing transaction: revert'
-  return errorMessage ? RegExp(`^${prefix + ' ' + errorMessage}$`) : RegExp(`^${prefix}$`)
+  return errorMessage ? `${prefix + ' ' + errorMessage}` : prefix
 }
 
 export interface JSONRPCRequest {
