f8badc1d3c57aeb55c786afcf2f19b30904cac2c
Agustin Aguilar <Agusxrun@gmail.com>
2020-05-21 17:14:01 +0200

    Inline ECDSA malleability checks for gas savings

--- contracts/utils/SignatureValidator.sol
+++ contracts/utils/SignatureValidator.sol
@@ -3,7 +3,6 @@ pragma experimental ABIEncoderV2;
 
 import "../interfaces/IERC1271Wallet.sol";
 
-import "./ECDSA.sol";
 import "./LibBytes.sol";
 
 /**
@@ -65,17 +64,38 @@ contract SignatureValidator {
     bytes32 r = _signature.readBytes32(0);
     bytes32 s = _signature.readBytes32(32);
 
+    // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+    // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+    // the valid range for s in (281): 0 < s < secp256k1n ÷ 2 + 1, and for v in (282): v ∈ {27, 28}. Most
+    // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+    //
+    // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+    // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+    // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+    // these malleable signatures as well.
+    //
+    // Source OpenZeppelin
+    // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/cryptography/ECDSA.sol
+
+    if (uint256(s) > 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0) {
+      revert("SignatureValidator#recoverSigner invalid signature 's' value");
+    }
+
+    if (v != 27 && v != 28) {
+      revert("SignatureValidator#recoverSigner invalid signature 'v' value");
+    }
+
     // Signature using EIP712
     if (signatureType == SignatureType.EIP712) {
-      signer = ECDSA.recover(_hash, r, s, v);
+      signer = ecrecover(_hash, v, r, s);
 
     // Signed using web3.eth_sign() or Ethers wallet.signMessage()
     } else if (signatureType == SignatureType.EthSign) {
-      signer = ECDSA.recover(
+      signer = ecrecover(
         keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", _hash)),
+        v,
         r,
-        s,
-        v
+        s
       );
 
     } else {
