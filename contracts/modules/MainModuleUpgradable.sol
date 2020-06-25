a64bc785173a7d7260fcc764c29e540fb5a190d9
PhABC <ph.castonguay@gmail.com>
2020-06-25 11:06:55 -0400

    Comments for inheritance

--- contracts/modules/MainModuleUpgradable.sol
+++ contracts/modules/MainModuleUpgradable.sol
@@ -8,6 +8,13 @@ import "./commons/ModuleUpdate.sol";
 import "./commons/ModuleCreator.sol";
 
 
+/**
+ * @notice Contains the core functionality arcadeum wallets will inherit with
+ *         the added functionality that the main-module can be changed.
+ * @dev If using a new main module, developpers must ensure that all inherited
+ *      contracts by the mainmodule don't conflict and are accounted for to be
+ *      supported by the supportsInterface method.
+ */
 contract MainModuleUpgradable is
   ModuleAuthUpgradable,
   ModuleCalls,
@@ -18,6 +25,9 @@ contract MainModuleUpgradable is
   /**
    * @notice Query if a contract implements an interface
    * @param _interfaceID The interface identifier, as specified in ERC-165
+   * @dev If using a new main module, developpers must ensure that all inherited
+   *      contracts by the mainmodule don't conflict and are accounted for to be
+   *      supported by the supportsInterface method.
    * @return `true` if the contract implements `_interfaceID`
    */
   function supportsInterface(
