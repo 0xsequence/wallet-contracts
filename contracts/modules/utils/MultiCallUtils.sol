37e6e65df0f4c6a274289804bab3eacffc86ac6d
Agustin Aguilar <agusgit@pm.me>
2023-03-16 08:27:18 +0000

    Fix multicall blockNumber wrong value

--- contracts/modules/utils/MultiCallUtils.sol
+++ contracts/modules/utils/MultiCallUtils.sol
@@ -61,7 +61,7 @@ contract MultiCallUtils {
   }
 
   function callBlockNumber() external view returns (uint256) {
-    return block.gaslimit;
+    return block.number;
   }
 
   function callTimestamp() external view returns (uint256) {
