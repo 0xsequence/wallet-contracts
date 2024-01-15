8d18380d22243d7d01427873eef43ad04bb80ac6
Agusx1211 <agusgit@pm.me>
2024-01-15 18:06:21 +0000

    Implement more machine codes

--- foundry_test/modules/utils/L2CompressorHuff.t.sol
+++ foundry_test/modules/utils/L2CompressorHuff.t.sol
@@ -13,6 +13,8 @@ interface L2CompressorImps {
     uint256 rindex,
     uint256 size
   ) external pure returns (uint256, uint256);
+
+  function testReadBytes32(bytes32 _a, bytes32 _b, uint256 rindex, uint256 windex, uint256 flag) external pure returns (uint256, uint256, bytes32);
 }
 
 contract L2CompressorHuffTests is Test {
@@ -28,7 +30,9 @@ contract L2CompressorHuffTests is Test {
   }
 
   function test_load_dynamic_size() external {
-    (uint256 size, uint256 rindex) = imp.testLoadDynamicSize(
+    uint256 size; uint256 rindex;
+
+    (size, rindex) = imp.testLoadDynamicSize(
       bytes32(0x082366f82de6ef3a1d439d0adbfa2ee606f86c2774b4d946f895dfc1f88443a2),
       bytes32(0),
       4,
@@ -41,8 +45,36 @@ contract L2CompressorHuffTests is Test {
     (size, rindex) = imp.testLoadDynamicSize(
       bytes32(0x082366f82de6ef3a1d439d0adbfa2ee606f86c2774b4d946f895dfc1f88443a2),
       bytes32(0),
-      4 + 32 - 3,
-      2219024896
+      4 + 0,
+      2
     );
+
+    assertEq(size, 2083);
+    assertEq(rindex, 4 + 2);
+
+    (size, rindex) = imp.testLoadDynamicSize(
+      bytes32(0x082366f82de6ef3a1d439d0adbfa2ee606f86c2774b4d946f895dfc1f88443a2),
+      bytes32(0),
+      4 + 2,
+      4
+    );
+
+    assertEq(size, 1727540710);
+    assertEq(rindex, 4 + 4 + 2);
+  }
+
+  function test_load_bytes32() external {
+    uint256 windex; uint256 rindex; bytes32 value;
+
+    (windex, rindex, value) = imp.testReadBytes32(
+      bytes32(0x020166f82de6ef3a1d439d0adbfa2ee606f86c2774b4d946f895dfc1f88443a2),
+      bytes32(0),
+      4,
+      0,
+      0x23
+    );
+
+    assertEq(windex, 32);
+    assertEq(rindex, 4 + 2);
   }
 }
